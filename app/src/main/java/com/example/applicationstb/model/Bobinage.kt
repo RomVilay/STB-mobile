1f2650548c0b06f19a39c19ff88f0be4dec24781
Romain V <romain.vilayleck@alf-environnement.com>
2021-03-05 16:59:26 +0100

    feat: vue chantier

--- app/src/main/java/com/example/applicationstb/model/Bobinage.kt
+++ app/src/main/java/com/example/applicationstb/model/Bobinage.kt
@@ -1,4 +1,37 @@
 package com.example.applicationstb.model
 
-class Bobinage {
+import android.media.Image
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDateTime
+
+class Bobinage( numDevis: String,
+                numChantier: String,
+                client: Client,
+                contact: String,
+                telContact: Long,
+                techniciens: Array<User>,
+                resp: User,
+                var marque:String,
+                val type:String,
+                val puissance:Long,
+                var vitesse: Long,
+                var tension: Long,
+                var phases:String,
+                var frequence:String,
+                var courant:String,
+                val callage:Boolean) : Fiche(numDevis, numChantier, client, contact, telContact, techniciens, resp){
+
+                    var sectionsFils = mapOf<Long,Int>();
+                    var nbSpires = 0;
+                    var Resistance = mapOf<String,Long>()
+                    var Isolement1 = mapOf<String,Long>()
+                    var Isolement2 = mapOf<String,Long>()
+                    @RequiresApi(Build.VERSION_CODES.O)
+                    var date = LocalDateTime.now() ;
+                    var Observation = ""
+                    var schema = arrayOf<Image>()
+
+
+
 }
