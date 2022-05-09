8f932a223ec8dace43c263ebbcd4d278ae654c43
Romain V <romain.vilayleck@alf-environnement.com>
2022-05-09 11:30:29 +0200

    feat: fiche remontage triphase V2

--- app/src/main/java/com/example/applicationstb/model/Remontage.kt
+++ app/src/main/java/com/example/applicationstb/model/Remontage.kt
@@ -83,48 +83,6 @@ class RemontageTriphase(
      var isolementPorteBalaisV: Float?,
      var isolementPorteBalaisOhm: Float?,
     // essais dynamiques
-     var tensionStator: Boolean?,
-     var tensionStatorU: Float?,
-     var tensionStatorV: Float?,
-     var tensionStatorW: Float?,
-     var tensionInducteurs: Boolean?,
-     var tensionInducteursU: Float?,
-     var tensionInducteursV: Float?,
-     var tensionInducteursW: Float?,
-     var intensiteStator: Boolean?,
-     var intensiteStatorU: Float?,
-     var intensiteStatorV: Float?,
-     var intensiteStatorW: Float?,
-     var intensiteInducteurs: Boolean?,
-     var intensiteInducteursU: Float?,
-     var intensiteInducteursV: Float?,
-     var intensiteInducteursW: Float?,
-     var tensionInduit: Boolean?,
-     var tensionInduitU: Float?,
-     var tensionInduitV: Float?,
-     var tensionInduitW: Float?,
-     var tensionRotor: Boolean?,
-     var tensionRotorU: Float?,
-     var tensionRotorV: Float?,
-     var tensionRotorW: Float?,
-     var intensiteInduit: Boolean,
-     var intensiteInduitU: Float?,
-     var vitesseU: Float?,
-     var puissanceU: Float?,
-     var dureeEssai: Float?,
-     var sensRotation: Int?,
-
-//essais vibratoires
-     var vitesse1V: Float?,  // vitesse 1v
-     var acceleration1V: Float?,  //accélération 1v
-     var vitesse2V: Float?,  // vitesse 2v
-     var acceleration2V: Float?,  //accélération 2v
-     var vitesse1H: Float?,  // vitesse 1H
-     var acceleration1H: Float?,  //accélération 1H
-     var vitesse2H: Float?,  // vitesse 2H
-     var acceleration2H: Float?,  //accélération 2H
-     var vitesse2A: Float?,  // vitesse 2A
-     var acceleration2A: Float?,  //accélération 2A
     var isolementPhaseMasse: Float?,
     var isolementPhase: Float?,
     var resistanceStatorU: Float?,
@@ -141,7 +99,25 @@ class RemontageTriphase(
     var isolementPhaseStatorUW: Float?,
     var isolementPhaseRotorUV: Float?,
     var isolementPhaseRotorVW: Float?,
-    var isolementPhaseRotorUW: Float?
+    var isolementPhaseRotorUW: Float?,
+//essais vibratoires
+     var vitesse1V: Float?,  // vitesse 1v
+     var acceleration1V: Float?,  //accélération 1v
+     var vitesse2V: Float?,  // vitesse 2v
+     var acceleration2V: Float?,  //accélération 2v
+     var vitesse1H: Float?,  // vitesse 1H
+     var acceleration1H: Float?,  //accélération 1H
+     var vitesse2H: Float?,  // vitesse 2H
+     var acceleration2H: Float?,  //accélération 2H
+     var vitesse2A: Float?,  // vitesse 2A
+     var acceleration2A: Float?,  //accélération 2A
+     var tensionU:Float?,
+     var tensionV:Float?,
+     var tensionW:Float?,
+     var intensiteU:Float?,
+     var intensiteV:Float?,
+     var intensiteW:Float?,
+     var dureeEssai: Float?
 ) : Remontage(
     idFiche,
     numDevis,
@@ -182,36 +158,6 @@ class RemontageTriphase(
             verificationIsolementPorteBalais,
             isolementPorteBalaisV,
             isolementPorteBalaisOhm,
-            tensionStator,
-            tensionStatorU,
-            tensionStatorV,
-            tensionStatorW,
-            tensionInducteurs,
-            tensionInducteursU,
-            tensionInducteursV,
-            tensionInducteursW,
-            intensiteStator,
-            intensiteStatorU,
-            intensiteStatorV,
-            intensiteStatorW,
-            intensiteInducteurs,
-            intensiteInducteursU,
-            intensiteInducteursV,
-            intensiteInducteursW,
-            tensionInduit,
-            tensionInduitU,
-            tensionInduitV,
-            tensionInduitW,
-            tensionRotor,
-            tensionRotorU,
-            tensionRotorV,
-            tensionRotorW,
-            intensiteInduit,
-            intensiteInduitU,
-            vitesseU,
-            puissanceU,
-            dureeEssai,
-            sensRotation,
             vitesse1V,  // vitesse 1v
             acceleration1V,  //accélération 1v
             vitesse2V,  // vitesse 2v
@@ -238,7 +184,14 @@ class RemontageTriphase(
             isolementPhaseStatorUW,
             isolementPhaseRotorUV,
             isolementPhaseRotorVW,
-            isolementPhaseRotorUW
+            isolementPhaseRotorUW,
+            tensionU,
+            tensionV,
+            tensionW,
+            intensiteU,
+            intensiteV,
+            intensiteW,
+            dureeEssai,
         )
 
     }
